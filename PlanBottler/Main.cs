15c134884cf353d096c1e74ef2f71b57c7b73db3
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2019-09-29 11:19:06 +0200

    Add StyleCop

--- PlanBottler/Main.cs
+++ PlanBottler/Main.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace PlanBottler
 {
+    /// <summary>
+    /// Main Form.
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.Form" />
     public partial class Main : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Main"/> class.
+        /// </summary>
         public Main()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
--- PlanBottler/Program.cs
+++ PlanBottler/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PlanBottler
 {
-    static class Program
+    /// <summary>
+    /// Program.
+    /// </summary>
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
